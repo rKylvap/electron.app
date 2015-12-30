9beb17cc64a9fd4383e2aa3060bdb00deddc16c3
ipavlyk <pvihor@gmail.com>
2015-12-30 21:46:00 +0200

    enabled html5mode

--- public/js/config.js
+++ public/js/config.js
@@ -1,19 +1,21 @@
 angular.
     module('electronAngularDemo').
-    config(function($stateProvider, $urlRouterProvider){
+    config(function($stateProvider, $urlRouterProvider, $locationProvider){
 
     $urlRouterProvider.otherwise("/");
+
+    $locationProvider.html5Mode({ enabled: true, requireBase: false});
 
     $stateProvider
         .state('home', {
             url: "/",
-            templateUrl: __dirname.concat('/core/coreTmpl.html'),
+            templateUrl: '/js/core/coreTmpl.html',
             controller: 'coreCtrl',
             controllerAs: 'ctrl'
         })
         .state('about', {
             url: '/about',
-            templateUrl: __dirname.concat('/about/aboutTmpl.html'),
+            templateUrl: '/js/about/aboutTmpl.html',
             controller: 'aboutCtrl',
             controllerAs: 'ctrl'
         });
--- server.js
+++ server.js
@@ -1,24 +1,34 @@
 var express = require('express');
 var app = express();
+var server = require('http').createServer(app);
 var router = express.Router();
-var morgan = require('morgan');
-var index = require('./routes/index')(router);
+//var morgan = require('morgan');
 var path = require('path');
+var favicon = require('serve-favicon');
 
-
+var index = require('./routes/index')(router);
 
 app.engine('jade', require('jade').__express);
 app.set('view engine', 'jade');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(__dirname));
-//app.use(express.static(__dirname + '/public'));
-//app.use('/bower_components',  express.static(__dirname));
+app.use(express.static(__dirname + '/public'));
+app.use('/js',  express.static(__dirname + '/js'));
+app.use('/css',  express.static(__dirname + '/css'));
+app.use('/views',  express.static(__dirname + '/views'));
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+
+app.use(favicon(__dirname + '/public/favicon.ico'));
 
 app.use('/', index);
 
-app.listen('3000', function(){
-    console.log('Listening on 3000!');
+app.all('/*', function(req, res, next){
+    res.sendFile('/views/layout.html', { root: __dirname });
+});
+
+server.listen(process.env.PORT || 3000, function(){
+    console.log('Listening on '.concat(server.address().port));
 });
 
 module.exports = app;
